84ff4b7b3880409904c528c4d98f1d16910d3fba
Aldray Narvaez <aldray.devsoft@gmail.com>
2024-08-01 12:08:01 -0500

    first commit

---
+++ src/main/java/dev/aldrinho/practicaltestgml/PracticalTestGmlApplication.java
@@ -0,0 +1,13 @@
+package dev.aldrinho.practicaltestgml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalTestGmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticalTestGmlApplication.class, args);
+    }
+
+}
